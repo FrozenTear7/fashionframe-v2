d9969a26e3d4e1f7b12f43f4c0c35a98742278bb
Paweł Mendroch <pawelmendroch7@gmail.com>
2021-03-23 13:25:08 +0100

    Add fetch multiple setups by user

--- src/controllers/setupController.ts
+++ src/controllers/setupController.ts
@@ -12,81 +12,35 @@ export const createSetup = async (
   res: Response,
   next: NextFunction
 ): Promise<void> => {
-  const {
-    user,
-    attachments,
-    syandana,
-    colorScheme,
-    name,
-    description,
-    frame,
-    helmet,
-    skin,
-    screenshot,
-  } = req.body;
+  const { user, attachments, syandana, colorScheme, ...setup } = req.body;
 
   const session = await mongoose.startSession();
 
   try {
-    const createdUser = new User({ username: user.username });
+    const createdUser = new User(user);
 
-    const createdAttachmentsColorScheme = new ColorScheme({
-      primary: attachments.colorScheme.primary,
-      secondary: attachments.colorScheme.secondary,
-      tertiary: attachments.colorScheme.tertiary,
-      accents: attachments.colorScheme.accents,
-      emmissive1: attachments.colorScheme.emmissive1,
-      emmissive2: attachments.colorScheme.emmissive2,
-      energy1: attachments.colorScheme.energy1,
-      energy2: attachments.colorScheme.energy2,
-    });
+    const createdAttachmentsColorScheme = new ColorScheme(
+      attachments.colorScheme
+    );
     const createdAttachments = new Attachments({
+      ...attachments,
       colorScheme: createdAttachmentsColorScheme,
-      chest: attachments.chest,
-      leftArm: attachments.leftArm,
-      rightArm: attachments.rightArm,
-      leftLeg: attachments.leftLeg,
-      rightLeg: attachments.rightLeg,
-      ephemera: attachments.ephemera,
     });
 
-    const createdSyandanaColorScheme = new ColorScheme({
-      primary: syandana.colorScheme.primary,
-      secondary: syandana.colorScheme.secondary,
-      tertiary: syandana.colorScheme.tertiary,
-      accents: syandana.colorScheme.accents,
-      emmissive1: syandana.colorScheme.emmissive1,
-      emmissive2: syandana.colorScheme.emmissive2,
-      energy1: syandana.colorScheme.energy1,
-      energy2: syandana.colorScheme.energy2,
-    });
+    const createdSyandanaColorScheme = new ColorScheme(syandana.colorScheme);
     const createdSyandana = new Syandana({
+      ...syandana,
       colorScheme: createdSyandanaColorScheme,
-      name: syandana.name,
     });
 
-    const createdColorScheme = new ColorScheme({
-      primary: colorScheme.primary,
-      secondary: colorScheme.secondary,
-      tertiary: colorScheme.tertiary,
-      accents: colorScheme.accents,
-      emmissive1: colorScheme.emmissive1,
-      emmissive2: colorScheme.emmissive2,
-      energy1: colorScheme.energy1,
-      energy2: colorScheme.energy2,
-    });
+    const createdColorScheme = new ColorScheme(colorScheme);
 
     const createdSetup = new Setup({
       user: createdUser,
       attachments: createdAttachments,
       syandana: createdSyandana,
       colorScheme: createdColorScheme,
-      name: name,
-      description: description,
-      frame: frame,
-      helmet: helmet,
-      skin: skin,
-      screenshot: screenshot,
+      ...setup,
     });
 
     await session.withTransaction(async () => {
@@ -99,16 +53,35 @@ export const createSetup = async (
       await createdSetup.save({ session });
     });
 
-    console.log(createdSetup);
     res.send({
       data: createdSetup,
       message: `Successfully created setup`,
     });
   } catch (e) {
-    console.log(e.message);
+    console.log(e);
     next(new HttpException(400, `Error while creating setup`));
   } finally {
     session.endSession();
+  }
+};
+
+export const getSetupsByUserId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const userId = req.params.userId;
+
+  try {
+    const setups = await Setup.find({ 'user._id': userId });
+
+    res.send({
+      data: setups,
+      message: `Successfully fetched setups`,
+    });
+  } catch (e) {
+    console.log(e);
+    next(new HttpException(404, `Error while fetching setups`));
   }
 };
 
@@ -121,12 +94,17 @@ export const getSetupById = async (
 
   try {
     const setup = await Setup.findById(id);
-    console.log(setup);
-    res.send({
-      data: setup,
-      message: `Successfully deleted setup`,
-    });
+
+    if (setup) {
+      res.send({
+        data: setup,
+        message: `Successfully fetched setup`,
+      });
+    } else {
+      next(new HttpException(404, `Setup does not exist`));
+    }
   } catch (e) {
+    console.log(e);
     next(new HttpException(404, `Error while fetching setup`));
   }
 };
@@ -139,9 +117,18 @@ export const deleteSetupById = async (
   const id = req.params.id;
 
   try {
-    await Setup.findByIdAndDelete(id);
-    res.send({ message: `Successfully deleted setup` });
+    const setup = await Setup.findByIdAndDelete(id);
+
+    if (setup) {
+      res.send({
+        data: setup,
+        message: `Successfully deleted setup`,
+      });
+    } else {
+      next(new HttpException(404, `Setup does not exist`));
+    }
   } catch (e) {
+    console.log(e);
     next(new HttpException(404, `Error while deleting setup`));
   }
 };
--- src/routes/setups.ts
+++ src/routes/setups.ts
@@ -2,13 +2,14 @@ import {
   createSetup,
   getSetupById,
   deleteSetupById,
+  getSetupsByUserId,
 } from './../controllers/setupController';
 import express from 'express';
 
 const router = express.Router();
 
 router.post('/', createSetup);
-// router.get('/:userId', getSetupsByUserId);
+router.get('/user/:userId', getSetupsByUserId);
 router.get('/:id', getSetupById);
 // router.put('/:id', updateSetupById);
 router.delete('/:id', deleteSetupById);
