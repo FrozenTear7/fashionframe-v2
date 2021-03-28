c33d967392fd31bc20d54e571c66e37dc81f4f57
Paweł Mendroch <pawelmendroch7@gmail.com>
2021-03-28 22:22:53 +0200

    Add helmet safety to the server

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,11 +3,13 @@ import auth from './routes/auth';
 import warframeData from './routes/warframeData';
 import setups from './routes/setups';
 import errorMiddleware from './middleware/errorMiddleware';
+import helmet from 'helmet';
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(helmet());
 
 app.use('/users', auth);
 app.use('/api', warframeData);
