35b8f243e6ced53268ec28a1d19d0e07ba4b8886
Paweł Mendroch <pawelmendroch7@gmail.com>
2021-04-02 14:53:04 +0200

    MaterialUI Header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,55 +1,85 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
-import { useUserContext } from '../../UserContext';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 import { signOut } from '../../utils/auth';
+import { useUserContext } from '../../UserContext';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
 
 const Header: React.VFC = () => {
+  const classes = useStyles();
   const { user, setUser } = useUserContext();
 
   return (
     <div className="Header">
-      HEADER - User: {user?.username}
-      <ul>
-        <li>
-          <Link to="/">Homepage</Link>
-        </li>
-        {!user && (
-          <li>
-            <Link to="/signup">Sign up</Link>
-          </li>
-        )}
-        {!user && (
-          <li>
-            <Link to="/signin">Sign in</Link>
-          </li>
-        )}
-        {user && (
-          <button
-            type="button"
-            onClick={async (): Promise<void> => {
-              await signOut();
-              setUser(null);
-            }}
-          >
-            Sign out
-          </button>
-        )}
-        <li>
-          <Link to="/setups">Setups</Link>
-        </li>
-        <li>
-          <Link to="/setups/create">Create new setup</Link>
-        </li>
-        <li>
-          <Link to="/setups/1">Setup 1</Link>
-        </li>
-        <li>
-          <Link to="/user/1">User 1</Link>
-        </li>
-        <li>
-          <Link to="/profile">Own profile</Link>
-        </li>
-      </ul>
+      <AppBar position="static">
+        <Toolbar>
+          <Button color="inherit" component={Link} to="/setups">
+            Setups
+          </Button>
+          {user && (
+            <Button color="inherit" component={Link} to="/setups/create">
+              Create new setup
+            </Button>
+          )}
+          <Typography variant="h6" className={classes.title}>
+            Fashionframe
+          </Typography>
+          {user ? (
+            <>
+              <>
+                <IconButton
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  color="inherit"
+                  component={Link}
+                  to="/profile"
+                >
+                  <AccountCircle />
+                </IconButton>
+                {user.username}
+              </>
+              <Button
+                color="inherit"
+                onClick={async (): Promise<void> => {
+                  await signOut();
+                  setUser(null);
+                }}
+              >
+                Sign out
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button color="inherit" component={Link} to="/signin">
+                Sign in
+              </Button>
+              <Button color="inherit" component={Link} to="/signup">
+                Sign up
+              </Button>
+            </>
+          )}
+        </Toolbar>
+      </AppBar>
     </div>
   );
 };
--- client/src/components/Utils/Loading.tsx
+++ client/src/components/Utils/Loading.tsx
@@ -1,7 +1,12 @@
+import { CircularProgress } from '@material-ui/core';
 import * as React from 'react';
 
 const Loading: React.VFC = () => {
-  return <div className="Loading">Loading...</div>;
+  return (
+    <div className="Loading">
+      <CircularProgress />
+    </div>
+  );
 };
 
 export default Loading;
